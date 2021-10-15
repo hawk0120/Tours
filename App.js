0d3e5a52bf9f02f65609a1643b244f22fa533b83
Brady <hawk0120@algonquinlive.com>
2021-10-14 20:42:10 -0500

    Fixed onBackward and onForward buttons

--- App.js
+++ App.js
@@ -1,32 +1,81 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, FlatList, Alert, ImageBackground} from 'react-native';
-import ImageExpander from './components/ImageExpander';
+import {View,
+       Text, 
+       StyleSheet, 
+       TouchableOpacity, 
+       Image,
+       Alert, 
+       ImageBackground} from 'react-native';
 import Header from './components/Header';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
-
+import { tourItems } from './src/tourList';
 
 const App = () => {
+
+
+  const [currentItem, setCurrentItem] = useState(0);
+  const onStartPress = async (e) => {
+    //  try {
+    // const path = 'file://' + dirs + '/' + tourItem[currentItem].imgPath;
+    //  } catch (err) {
+    //    console.log(err);
+    //  }
+
+   };
+  const onBackward = async (e) => {
+        // Subtract 1  tourItems.id
+
+        let curr_item = tourItems[currentItem];
+        let curr_index = tourItems.indexOf(curr_item);
+
+        if(curr_index == 0) {
+        setCurrentItem((tourItems.length - 1));
+        }  else {
+        setCurrentItem((curr_Item) => currentItem - 1);
+        } // End of if
+    }; // End of onBackward
+
+  const onForward = async (e) => {
+    // add 1  tourItems.id
+
+    let curr_item = tourItems[currentItem];
+    let curr_index = tourItems.indexOf(curr_item);
+  
+    if((curr_index + 1) == tourItems.length) {
+      setCurrentItem(0);
+    }  else {
+      setCurrentItem((curr_Item) => currentItem + 1);
+    } // End of if
+  } // End of onForward
+
 
   return (
 <View style={styles.mainContainer}>  
     <View style={styles.container}>
       <Header />    
-      <ImageExpander />
+      <Image 
+      source={{
+        uri: tourItems[currentItem].imgPath
+      }} 
+      style={styles.img} 
+      />
+     
+     
     </View>
     <View style={styles.buttons}>
-    <TouchableOpacity onPress={() => {}}> 
+    <TouchableOpacity onPress={() => onBackward()}> 
          <Icon 
          name="backward"
          size={50}
         color="#001219"/>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => {}} >
+    <TouchableOpacity onPress={() => onStartPress()} >
            <Icon 
             name="play-circle"
             size={60}
             color="#001219"/>      
    </TouchableOpacity>
-    <TouchableOpacity onPress={() => {}}>
+    <TouchableOpacity onPress={() => onForward()}>
          <Icon 
          name="fast-forward"
          size={50}
@@ -34,7 +83,7 @@ const App = () => {
           />
     </TouchableOpacity>
     </View>
-    <Text style={styles.title}>Location Name</Text>
+    <Text style={styles.title}>{tourItems[currentItem].location}</Text>
   </View>
   );
 };
@@ -61,7 +110,15 @@ const styles = StyleSheet.create({
     position: 'absolute',
     marginVertical: '50%',
     marginHorizontal: '50%'
+  },
+  img: {
+    alignContent: 'center',
+    width: '100%',
+    height: 700,
+    opacity: 0.8
   }
+
 });
+
 
 export default App;
---
+++ src/tourList.js
@@ -0,0 +1,21 @@
+
+
+
+export const tourItems = [
+    {
+      location: "Winnipeg",
+      imgPath: "https://firebasestorage.googleapis.com/v0/b/tourserver-f9e0e.appspot.com/o/img1.jpg?alt=media&token=1c9b4bb3-69a9-4c3c-8a61-ac91c6cd7a2c",
+      id: '0'
+  
+    },
+    {
+      location: "Kenora",
+      imgPath: "https://firebasestorage.googleapis.com/v0/b/tourserver-f9e0e.appspot.com/o/img2.jpg?alt=media&token=1c9e169e-aa50-46a0-97d8-90f0f2377b7b",
+      id: '1'
+    },
+    { location: "Canada",
+      imgPath: "https://firebasestorage.googleapis.com/v0/b/tourserver-f9e0e.appspot.com/o/img1.jpg?alt=media&token=1c9b4bb3-69a9-4c3c-8a61-ac91c6cd7a2c",
+      id: '2'
+    }
+  ]
+  
